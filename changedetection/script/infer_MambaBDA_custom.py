678342ca17ec46be6a39a06a6320570d1cec3a7c
koleshjr <stevekolesh975@gmail.com>
2024-12-23 15:01:22 +0300

    Confidence thresh to the help??

--- changedetection/script/infer_MambaBDA_custom.py
+++ changedetection/script/infer_MambaBDA_custom.py
@@ -202,7 +202,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--model_type', type=str, default='MambaBDA_Tiny')
     parser.add_argument('--result_saved_path', type=str, default='../results')
-    parser.add_argument('--choice', type='str', default='mean')
+    parser.add_argument('--choice', type=str, default='mean')
     parser.add_argument("--conf_threshold", type=float, default=0.0, help="conf Threshold to exclude small regions by mean confidence")
     # Add other arguments here as needed
     parser.add_argument('--resume', type=str)
